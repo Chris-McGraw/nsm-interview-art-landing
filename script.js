9821435a71563a9a4386fcdc09541cf937e50c7b
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-10-27 17:51:27 -0400

    index-update-32 / style-update-36 / script-update-1
    edits to #section-3-latest-work-menu / initial addition of workTypeButtonSwap() functionality

--- script.js
+++ script.js
@@ -1,21 +1,68 @@
 $(document).ready(function() {
 
 /* ------------------------- VARIABLE DECLARATIONS ------------------------- */
-  var $selectionHome = $("#selection-home");
-  var $selectionWhatWeDo = $("#selection-what-we-do");
-  var $selectionTestimonial = $("#selection-testimonial");
-  var $selectionContact = $("#selection-contact");
 
   var $menuHome = $("#menu-home");
+  var $selectionHome = $("#selection-home");
   var $menuWhatWeDo = $("#menu-what-we-do");
+  var $selectionWhatWeDo = $("#selection-what-we-do");
   var $menuTestimonial = $("#menu-testimonial");
+  var $selectionTestimonial = $("#selection-testimonial");
   var $menuContact = $("#menu-contact");
+  var $selectionContact = $("#selection-contact");
+
+  var $workMenuPhotographyButton = $("#work-menu-photography-button");
+  var $workMenuPhotography = $("#work-menu-photography");
+  var $workMenuDesignButton = $("#section-3-menu");
+  var $workMenuDesign = $("#work-menu-design");
+  var $workMenuPrintButton = $("#work-menu-print-button");
+  var $workMenuPrint = $("#work-menu-print");
+
+
+  workTypeButtonActive = "photography";
+
 
 /* ------------------------- FUNCTION DECLARATIONS ------------------------- */
+
+  function workTypeButtonSwap() {
+    if(workTypeButtonActive === "photography") {
+      $workMenuPhotographyButton.css("background-color", "rgb(43, 155, 43)");
+      $workMenuPhotography.css("color", "rgb(255, 255, 255)");
+
+      $workMenuDesignButton.css("background-color", "rgb(255, 255, 255)");
+      $workMenuDesign.css("color", "rgb(43, 155, 43)");
+
+      $workMenuPrintButton.css("background-color", "rgb(255, 255, 255)");
+      $workMenuPrint.css("color", "rgb(43, 155, 43)");
+    }
+
+    else if(workTypeButtonActive === "design") {
+      $workMenuDesignButton.css("background-color", "rgb(43, 155, 43)");
+      $workMenuDesign.css("color", "rgb(255, 255, 255)");
+
+      $workMenuPhotographyButton.css("background-color", "rgb(255, 255, 255)");
+      $workMenuPhotography.css("color", "rgb(43, 155, 43)");
+
+      $workMenuPrintButton.css("background-color", "rgb(255, 255, 255)");
+      $workMenuPrint.css("color", "rgb(43, 155, 43)");
+    }
+
+    else if(workTypeButtonActive === "print") {
+      $workMenuPrintButton.css("background-color", "rgb(43, 155, 43)");
+      $workMenuPrint.css("color", "rgb(255, 255, 255)");
+
+      $workMenuPhotographyButton.css("background-color", "rgb(255, 255, 255)");
+      $workMenuPhotography.css("color", "rgb(43, 155, 43)");
+
+      $workMenuDesignButton.css("background-color", "rgb(255, 255, 255)");
+      $workMenuDesign.css("color", "rgb(43, 155, 43)");
+    }
+  }
 
 
 /* ---------------------------- EVENT HANDLERS ---------------------------- */
 
+/* ---------------- SECTION 1 CONTAINER ---------------- */
   $menuHome.mouseenter(function() {
     $selectionHome.removeClass("hidden");
 
@@ -49,6 +96,46 @@ $(document).ready(function() {
     $selectionHome.addClass("hidden");
     $selectionWhatWeDo.addClass("hidden");
     $selectionTestimonial.addClass("hidden");
+  });
+
+
+/* ---------------- SECTION 3 CONTAINER ---------------- */
+  $workMenuPhotographyButton.on("click", function() {
+    workTypeButtonActive = "photography";
+
+    workTypeButtonSwap();
+  });
+
+  $workMenuPhotography.on("click", function() {
+    workTypeButtonActive = "photography";
+
+    workTypeButtonSwap();
+  });
+
+
+  $workMenuDesignButton.on("click", function() {
+    workTypeButtonActive = "design";
+
+    workTypeButtonSwap();
+  });
+
+  $workMenuDesign.on("click", function() {
+    workTypeButtonActive = "design";
+
+    workTypeButtonSwap();
+  });
+
+
+  $workMenuPrintButton.on("click", function() {
+    workTypeButtonActive = "print";
+
+    workTypeButtonSwap();
+  });
+
+  $workMenuPrint.on("click", function() {
+    workTypeButtonActive = "print";
+
+    workTypeButtonSwap();
   });
 
 
